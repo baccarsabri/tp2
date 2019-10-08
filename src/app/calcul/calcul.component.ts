ce15c4d1d5a36f24945fd7cf5458d199a5c55f48
mohamed-chebil <mohamed.chebil15@gmail.com>
2019-10-08 13:20:57 +0100

    exercice3 pas terminer

--- src/app/calcul/calcul.component.ts
+++ src/app/calcul/calcul.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    Oncarre()
+{
+  
+}
+   
+  
+}
 
   ngOnInit() {
   }
